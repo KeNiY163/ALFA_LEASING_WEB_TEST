0e61f7e46a87695c02bf3a27c2f90014fe5fa7d9
KeNiY163 <mishamotkov@gmail.com>
2023-11-23 20:13:21 +0400

    fix WebProvider.java

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -18,7 +18,7 @@ public interface WebConfig extends Config {
     Browser getBrowserName();
 
     @Key("browserVersion")
-    @DefaultValue("100.0")
+    @DefaultValue("116.0")
     String getBrowserVersion();
 
     @Key("browserSize")
